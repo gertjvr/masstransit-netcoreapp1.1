9e924bc9e3c47238fce0f42285b77af5bc37b369
Gert Jansen van Rensburg <gertjvr@gmail.com>
2017-06-26 21:45:42 +1000

    Initial commit

---
+++ src/Bus/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MassTransit;
+
+namespace Bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = MassTransit.Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://rabbit.localtest.me/"), h => {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                cfg.ReceiveEndpoint(host, "my_queue", endpoint =>
+                {
+                    endpoint.Handler<MyMessage>(async context =>
+                    {
+                        await Console.Out.WriteLineAsync($"Received: {context.Message.Value}");
+                    });
+                });
+            });
+
+            bus.Start();
+
+            bus.Publish(new MyMessage{Value = "Hello, World."});
+
+            Console.ReadLine();
+
+            bus.Stop();
+        }
+    }
+
+    class MyMessage
+    {
+        public string Value { get; set; }
+    }
+}
